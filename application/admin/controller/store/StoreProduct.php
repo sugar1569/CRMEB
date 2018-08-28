000a6f33a1265b967f1fa6a95ca92d93e9aba4fd
等风来 <136327134@qq.com>
2018-08-28 14:51:35 +0800

    修改bug

--- application/admin/controller/store/StoreProduct.php
+++ application/admin/controller/store/StoreProduct.php
@@ -222,7 +222,8 @@ class StoreProduct extends AuthController
         if(!$product) return Json::fail('数据不存在!');
         $form = Form::create(Url::build('update',array('id'=>$id)),[
             Form::select('cate_id','产品分类',explode(',',$product->getData('cate_id')))->setOptions(function(){
-                $list = CategoryModel::getTierList();
+		    $list = CategoryModel::getTierList();
+		    $menus=[];
                 foreach ($list as $menu){
                     $menus[] = ['value'=>$menu['id'],'label'=>$menu['html'].$menu['cate_name'],'disabled'=>$menu['pid']== 0];//,'disabled'=>$menu['pid']== 0];
                 }
